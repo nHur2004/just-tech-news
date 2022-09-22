db051990584f6fccdb24543ab75edc8e44510ce0
nHur2004 <nathan.hur04@gmail.com>
2022-09-22 15:54:50 -0600

    CRUD for posts-routes

--- models/Index.js
+++ models/Index.js
@@ -1,5 +1,12 @@
+const User = require("./User");
+const Post = require("./Post");
 
-const User = require('./User');
-const Post = require('./Post');
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User };
+module.exports = { User, Post };
